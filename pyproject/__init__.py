18d934f7c621956031f43164aefce16a27b752c5
Rotem Yaari <vmalloc@gmail.com>
2011-04-14 09:01:46 +0300

    Fix _find_all_subpackages

--- pyproject/__init__.py
+++ pyproject/__init__.py
@@ -3,18 +3,21 @@ from jinja2 import Template
 __version__ = "0.0.1"
 
 SETUP_FILE_TEMPLATE = Template("""
+import os
+import itertools
 from distutils.core import setup
 
 from {{projname}} import __version__ as VERSION
 
-def _find_packages(root_package):
-    root_dir = os.path.dirname(__file__)
-    package_root = os.path.abspath(os.path.join(root_dir, root_package))
-    yield root_package
-    for directory, dirnames, filenames in os.walk(package_root):
-        if "__init__.py" not in filenames:
-            del dirnames[:]
-        else:
+def _find_all_subpackages(*packages):
+    for root_package in packages:
+        root_dir = os.path.dirname(__file__)
+        package_root = os.path.abspath(os.path.join(root_dir, root_package))
+        yield root_package
+        for directory, dirnames, filenames in os.walk(package_root):
+            if "__init__.py" not in filenames:
+                del dirnames[:]
+                continue
             relative_package = os.path.relpath(directory, package_root).replace(os.path.sep, ".")
             if relative_package == '.':
                 continue
@@ -34,7 +37,7 @@ setup(name="{{projname}}",
       author_email="{{author_email}}",
       #url="your.url.here",
       version=VERSION,
-      packages=list(_find_packages("{{projname}}")),
+      packages=list(_find_all_subpackages("{{projname}}")),
       scripts=[],
       )
 
